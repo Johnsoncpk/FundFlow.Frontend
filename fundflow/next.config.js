156cff5b0d3578e5aa104fe69b90d68379446aac
Johnsoncpk <chong12319@gmail.com>
2023-10-07 00:50:14 +0800

    init

--- fundflow/next.config.js
+++ fundflow/next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
-})
+const nextConfig = {}
 
-module.exports = withPWA({
-  // config
-})
+module.exports = nextConfig
