de25ccae842ed287e885030ee13195d7ee4a22b8
Johnsoncpk <135187896+sleekflowjohnsonchong@users.noreply.github.com>
2024-01-29 21:17:25 +0800

    logic seperation

--- fundflow/src/components/templates/projects/Create.tsx
+++ fundflow/src/components/templates/projects/Create.tsx
@@ -4,7 +4,6 @@ import { Button, Flex, FormControl, FormErrorMessage, FormLabel, Heading, Input 
 import { Step, Steps, useSteps } from "chakra-ui-steps";
 import { Form1, Form2, Form3 } from "./StepForms";
 import { Field, Form, Formik, FormikProps } from 'formik';
-import { useState } from "react";
 
 const steps = [
   {
@@ -33,20 +32,10 @@ export const Create = ({
   const hasCompletedAllSteps = activeStep === steps.length;
   const bg = useColorModeValue("gray.200", "gray.700");
 
-  function validateName(value: string) {
-    let error
-    if (!value) {
-      error = 'Name is required'
-    } else if (value.toLowerCase() !== 'naruto') {
-      error = "Jeez! You're not a fan 😱"
-    }
-    return error
-  }
-
   return (
     <Flex flexDir="column" width="100%">
       <Formik
-        initialValues={{ name: 'Naruto' }}
+        initialValues={{ projectTitle: '', projectDescription:'' }}
         onSubmit={(values, actions) => {
           alert(JSON.stringify(values, null, 2))
           actions.setSubmitting(false)
@@ -54,15 +43,6 @@ export const Create = ({
         }}>
         {(props) => (
           <Form>
-            <Field name='name' validate={validateName}>
-              {({ field, form }) => (
-                <FormControl isInvalid={form.errors.name && form.touched.name}>
-                  <FormLabel>First name</FormLabel>
-                  <Input {...field} placeholder='name' />
-                  <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-                </FormControl>
-              )}
-            </Field>
             <Steps variant={variant} colorScheme="blue" activeStep={activeStep}>
               {steps.map(({ label, form }, index) => (
                 <Step label={label} key={label}>
@@ -75,46 +55,27 @@ export const Create = ({
             {hasCompletedAllSteps && (
               <Box sx={{ bg, my: 8, p: 8, rounded: "md" }}>
                 <Heading fontSize="xl" textAlign={"center"}>
-                  Woohoo! All steps completed! 🎉
+                  Woohoo! Project Created!!! 🎉
                 </Heading>
               </Box>
             )}
             <Flex width="100%" justify="flex-end" gap={4}>
-              {hasCompletedAllSteps ? (
-                <Flex>
-                  <Button
-                    size="sm" onClick={reset}>
-                    Reset
-                  </Button>
-                  <Button
-                    size="sm"
-                    colorScheme='teal'
-                    isLoading={props.isSubmitting}
-                    type='submit'
-                  >
-                    Submit
-                  </Button>
-                </Flex>
-              ) : (
-                <>
-                  <Button
-                    isDisabled={activeStep === 0}
-                    onClick={prevStep}
-                    size="sm"
-                    variant="ghost"
-                  >
-                    Prev
-                  </Button>
-                  {isLastStep ?
-                    <Button size="sm" colorScheme='teal' onClick={() => { props.handleSubmit }} isLoading={props.isSubmitting}>
-                      Finish
-                    </Button> :
-                    <Button size="sm" onClick={nextStep}>
-                      Next
-                    </Button>
-                  }
-                </>
-              )}
+              <Button
+                isDisabled={activeStep === 0}
+                onClick={prevStep}
+                size="sm"
+                variant="ghost"
+              >
+                Prev
+              </Button>
+              {isLastStep ?
+                <Button size="sm" colorScheme='teal' onClick={() => { props.handleSubmit }} isLoading={props.isSubmitting}>
+                  Finish
+                </Button> :
+                <Button size="sm" onClick={nextStep}>
+                  Next
+                </Button>
+              }
             </Flex>
           </Form>
         )}
--- fundflow/src/components/templates/projects/StepForms/Form1.tsx
+++ fundflow/src/components/templates/projects/StepForms/Form1.tsx
@@ -1,12 +1,37 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, FormControl, FormErrorMessage, FormLabel, Input } from "@chakra-ui/react";
+import { Field } from 'formik';
 
 export const Form1 = () => {
+  function validateName(value: string) {
+    let error
+    if (!value) {
+      error = 'Name is required'
+    } else if (value.toLowerCase() !== 'naruto') {
+      error = "Jeez! You're not a fan 😱"
+    }
+    return error
+  }
+
   return (
     <Flex flexDir="column" width="100%">
-       <Text>
-       1
-       </Text>
-  
+      <Field name='projectTitle' validate={validateName}>
+        {({ field, form }) => (
+          <FormControl isInvalid={form.errors.name && form.touched.name}>
+            <FormLabel>First name</FormLabel>
+            <Input {...field} placeholder='name' />
+            <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+          </FormControl>
+        )}
+      </Field>
+      <Field name='projectDescription' validate={validateName}>
+        {({ field, form }) => (
+          <FormControl isInvalid={form.errors.name && form.touched.name}>
+            <FormLabel>First name</FormLabel>
+            <Input {...field} placeholder='name' />
+            <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+          </FormControl>
+        )}
+      </Field>
     </Flex>
   );
 };
