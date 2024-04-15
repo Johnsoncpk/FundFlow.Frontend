abfa894ebe17a602c6581de778c3f70533e2f6b6
Johnsoncpk <75229912+Johnsoncpk@users.noreply.github.com>
2024-04-16 00:08:40 +0800

    update README.md

--- src/utils/wagmiConfig.ts
+++ src/utils/wagmiConfig.ts
@@ -3,7 +3,7 @@ import { createStorage, cookieStorage } from 'wagmi'
 import { polygonMumbai, hardhat, sepolia } from 'wagmi/chains'
 
 // Get projectId at https://cloud.walletconnect.com
-export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+export const projectId = process.env.WALLETCONNECT_PROJECT_ID
 
 if (!projectId) { throw new Error('Project ID is not defined') }
 
