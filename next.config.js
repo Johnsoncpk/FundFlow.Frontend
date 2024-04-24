9087d75d13213212db403f9d90ad525cecda98bc
Johnsoncpk <75229912+Johnsoncpk@users.noreply.github.com>
2024-04-24 21:34:07 +0800

    chore: add env

--- next.config.js
+++ next.config.js
@@ -8,6 +8,8 @@ const nextConfig = {
   env: {
     ENCRYPTION_KEY: process.env.ENCRYPTION_KEY,
     WALLETCONNECT_PROJECT_ID: process.env.WALLETCONNECT_PROJECT_ID,
+    CHAIN: process.env.CHAIN,
+    CONTRACT_ADDRESS: process.env.CONTRACT_ADDRESS,
   }
 };
 
